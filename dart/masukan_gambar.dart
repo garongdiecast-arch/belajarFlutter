8c883c540b079dc91f0464b3b18d433fce3a6ee4
garongdiecast-arch <garongdiecast@gmail.com>
2026-08-11 22:22:39 +0700

    Update masukan_gambar.dart

--- dart/masukan_gambar.dart
+++ dart/masukan_gambar.dart
@@ -20,3 +20,29 @@
     );
   }
 }
+//Masukan Gambar melalu link pada link image address
+// Pertama cari gambar dari internet
+//Lalu copy link image address
+//Setelah itu  ketikan perintiatah seperti dan masukan link pada url  berikut contoh Image(image:NetworkImage(url))
+import 'package:flutter/material.dart';
+
+class Radio extends StatelessWidget {
+  const Radio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.all(20)),
+            Text('Radio',style: TextStyle(fontSize: 30),),
+            Padding(padding: EdgeInsets.all(20)),
+            Image(image: NetworkImage('https://img.magnific.com/free-vector/smartphone-with-different-views-realistic-style_23-2147849864.jpg?semt=ais_test_b&w=740&q=80'))
+      
+          ],
+        ),
+      ),
+    );
+  }
+}
