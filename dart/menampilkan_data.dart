ca97879102a990337b049048aa3ab173ee4f8ca7
garongdiecast-arch <garongdiecast@gmail.com>
2026-08-26 21:07:57 +0700

    Update menampilkan_data.dart

--- dart/menampilkan_data.dart
+++ dart/menampilkan_data.dart
@@ -39,3 +39,74 @@ class Home extends StatelessWidget {
 }
 
 //Menampilkan data secara berulang dari JSON data
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main(){
+  runApp(MaterialApp(
+    title: "List View",
+    home: Home(),
+  ));
+}
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {  
+  List? dataJSON;
+  @override
+  void initState(){
+    super.initState();
+    ambildata();
+  }
+
+  Future<void> ambildata() async{
+    http.Response hasil =await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+      headers:{"Accept":"application/json"}
+    );
+    setState(() {
+      dataJSON=jsonDecode(hasil.body);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text('list data json'),
+      ),
+      body: ListView.builder(
+        itemCount: dataJSON?.length??0,
+        itemBuilder: (context, index){
+          return Container(
+            padding:EdgeInsets.all(12),
+            child:Card(
+              child: Container(
+                padding:EdgeInsets.all(12),
+                child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text(dataJSON![index]['title'],style: TextStyle(fontWeight: FontWeight.bold),),
+                  //const SizedBox(width: 10,),
+                  Text(dataJSON![index]['body']),
+
+                ],
+
+                ) 
+              ),
+            )
+            
+            
+          );
+        }
+
+      ),
+
+    );
+  }
+}
